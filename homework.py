87ea886e458a4b7589ca10cc21c74c2ce7e7ffd5
Alina Gorbenko <alinka3111@gmail.com>
2022-03-30 00:52:39 +0600

    PEP8

--- homework.py
+++ homework.py
@@ -9,8 +9,8 @@ class InfoMessage:
         self.calories = calories
 
     def get_message(self):
-        return f'Тип тренировки: {self.training_type}; Длительность: {"%.3f" %self.duration} ч.; ' \
-               f'Дистанция: {"%.3f" %self.distance} км; Ср. скорость: {"%.3f" %self.speed} км/ч; Потрачено ккал: {"%.3f" %self.calories}.'
+        return f'Тип тренировки: {self.training_type}; Длительность: {"%.3f" % self.duration} ч.; ' \
+               f'Дистанция: {"%.3f" % self.distance} км; Ср. скорость: {"%.3f" % self.speed} км/ч; Потрачено ккал: {"%.3f" % self.calories}.'
         pass
 
 
@@ -58,7 +58,8 @@ class Running(Training):
 
     # (18 * средняя_скорость - 20) * вес_спортсмена / M_IN_KM * время_тренировки_в_минутах
     def get_spent_calories(self, coffee_calorie_1=18, coffee_calorie_2=20) -> float:
-        return (coffee_calorie_1 * self.get_mean_speed() - coffee_calorie_2) * self.weight / self.M_IN_KM * (self.duration * 60)
+        return (coffee_calorie_1 * self.get_mean_speed() - coffee_calorie_2) * self.weight / self.M_IN_KM * (
+                    self.duration * 60)
         pass
 
     def show_training_info(self) -> InfoMessage:
@@ -79,7 +80,8 @@ class SportsWalking(Training):
         self.height = height
 
     def get_spent_calories(self):
-        return (0.035 * self.weight + (self.get_mean_speed() ** 2 // self.height) * 0.029 * self.weight) * self.duration * 60
+        return (0.035 * self.weight + (
+                    self.get_mean_speed() ** 2 // self.height) * 0.029 * self.weight) * self.duration * 60
         pass
 
     def show_training_info(self) -> InfoMessage:
